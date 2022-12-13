c0100eeea20b7c0894a22ddda0ee2a592bf5c27f
Vitória G <56117556+vitoriaGoncalves08@users.noreply.github.com>
2022-12-13 12:09:09 -0300

    :bug:image banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css'
 export const Banner = () =>{
     return(
         <header className="banner">
-        <img src="/imagens/Banner.png" alt="banner principal do organo"/>
+        <img src="./imagens/Banner.png" alt="banner principal do organo"/>
         </header>
     )
 }
